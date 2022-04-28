69f3c85581be55da20132a23f9774ddc7630d547
Xin Du <xdu@ucsd.edu>
2022-04-28 01:58:52 -0700

    get all tests

--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -19,6 +19,7 @@ export function typeCheck(source: string) : Type {
 
 // Modify run to use `importObject` (imported above) to use for printing
 export async function run(source: string) {
+  throw new Error(source);
   const program = source;
   try {
     const parsed = parse(program);
